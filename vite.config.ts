eef409caa15c266aa3c6ecb9997b421b8c289987
Shawn Campbell <jaegerpicker@gmail.com>
2025-10-04 15:19:44 -0400

    Workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/easton_boosters_website/', // Replace with your repository name
+  base: "/easton_boosters_website/", // Replace with your repository name
   plugins: [react()],
-})
+});
